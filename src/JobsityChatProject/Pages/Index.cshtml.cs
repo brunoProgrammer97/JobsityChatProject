2440e4a5aa090a73ea977b50f2c5af0057274e4a
Bruno Packers <packersapp2021@outlook.com>
2022-02-27 16:52:42 -0300

    login feature

--- src/JobsityChatProject/Pages/Index.cshtml.cs
+++ src/JobsityChatProject/Pages/Index.cshtml.cs
@@ -1,24 +1,60 @@
 ﻿using JobsityChatProject.Core.Models;
+using JobsityChatProject.Core.ServicesInterfaces;
 using JobsityChatProject.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using System;
+using System.Threading.Tasks;
 
 namespace JobsityChatProject.Pages
 {
     public class IndexModel : PageModel
     {
         [BindProperty]
-        public UserRegisterViewModel UserRegisterViewModel { get; set; }
-        public IndexModel()
+        public UserLoginViewModel UserLoginViewModel { get; set; }
+        private IChatUserServices _chatUserResvices;
+        public IndexModel(IChatUserServices chatUserResvices)
         {
-
+            _chatUserResvices = chatUserResvices;
         }
 
         public void OnGet()
         {
 
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var user = GetChatUserFromViewModel();
+                    var userRegistered = await _chatUserResvices.GetUserAsync(user);
+
+                    if (userRegistered != null)
+                    {
+                        await _chatUserResvices.SignInUserAsync(user, HttpContext);
+                        return Redirect("Account/Home");
+                    }                    
+                }
+
+                return Redirect("Index");
+            }
+            catch (Exception)
+            {
+                return Redirect("Index");
+            }
+        }
+
+        private ChatUser GetChatUserFromViewModel()
+        {
+            return new ChatUser()
+            {
+                UserName = UserLoginViewModel.UserName,
+                Password = UserLoginViewModel.Password
+            };
         }
     }
 }
