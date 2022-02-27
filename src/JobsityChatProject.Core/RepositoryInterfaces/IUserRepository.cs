5c41898d072116376baa044a227e759df05ce088
Bruno Packers <packersapp2021@outlook.com>
2022-02-27 12:44:14 -0300

    simples adjustments removing not used namespaces

--- src/JobsityChatProject.Core/RepositoryInterfaces/IUserRepository.cs
+++ src/JobsityChatProject.Core/RepositoryInterfaces/IUserRepository.cs
@@ -1,8 +1,4 @@
 ﻿using JobsityChatProject.Core.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace JobsityChatProject.Core.RepositoryInterfaces
