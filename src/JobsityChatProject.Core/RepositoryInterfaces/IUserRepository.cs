cce3a7c7345eb6fb9a04c038036208dcd8ae1f38
Bruno Packers <packersapp2021@outlook.com>
2022-02-27 12:13:31 -0300

    add initial structure to handler user data

---
+++ src/JobsityChatProject.Core/RepositoryInterfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using JobsityChatProject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobsityChatProject.Core.RepositoryInterfaces
+{
+    public interface IUserRepository
+    {
+        Task SaveUserAsync(User user);
+        Task GetUserAsync(User user);
+    }
+}
---
+++ src/JobsityChatProject.Infrastructure/Repository/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using JobsityChatProject.Core.Models;
+using JobsityChatProject.Core.RepositoryInterfaces;
+using System.Threading.Tasks;
+
+namespace JobsityChatProject.Infrastructure.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        public Task GetUserAsync(User user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task SaveUserAsync(User user)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
