8f465856b1f05f1e158b243b70ec6a540ac0dff3
Bruno Packers <packersapp2021@outlook.com>
2022-02-27 16:56:42 -0300

    register user feature

--- src/JobsityChatProject/Pages/Account/Register.cshtml.cs
+++ src/JobsityChatProject/Pages/Account/Register.cshtml.cs
@@ -1,3 +1,5 @@
+using JobsityChatProject.Core.Models;
+using JobsityChatProject.Core.ServicesInterfaces;
 using JobsityChatProject.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -9,7 +11,11 @@ namespace JobsityChatProject.Pages.Account
     {
         [BindProperty]
         public UserRegisterViewModel UserRegisterViewModel { get; set; }
-
+        private IChatUserServices _chatUserResvices;
+        public RegisterModel(IChatUserServices chatUserResvices)
+        {
+            _chatUserResvices = chatUserResvices;
+        }
         public void OnGet()
         {
         }
@@ -18,10 +24,24 @@ namespace JobsityChatProject.Pages.Account
         {
             if (ModelState.IsValid)
             {
-                    return Redirect("Account/Home");
+                var user = GetChatUserFromViewModel();
+                await _chatUserResvices.SaveUserAsync(user);
+                await _chatUserResvices.SignInUserAsync(user, HttpContext);
+
+                return Redirect("Account/Home");
             }
 
             return Redirect("Index");
+        }
+
+        private ChatUser GetChatUserFromViewModel()
+        {
+            return new ChatUser()
+            {
+                UserName = UserRegisterViewModel.UserName,
+                Password = UserRegisterViewModel.Password,
+                RepeatedPassword = UserRegisterViewModel.RepeatedPassword
+            };
         }
     }
 }
