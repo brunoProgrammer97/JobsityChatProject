69d785007f33ae13b7b50bb85617a90d9c36f0bb
Bruno Packers <packersapp2021@outlook.com>
2022-02-27 23:08:31 -0300

    ajustes

--- src/JobsityChatProject.Core/Hubs/GeneralChatHub.cs
+++ src/JobsityChatProject.Core/Hubs/GeneralChatHub.cs
@@ -2,17 +2,37 @@
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Caching.Memory;
 using System;
+using System.Globalization;
 using System.Threading.Tasks;
 
 namespace JobsityChatProject.Core.Hubs
 {
     public class GeneralChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        private readonly IChatMessageRepository _chatMessageRepository;
+        public GeneralChatHub(IChatMessageRepository chatMessageRepository)
         {
-            var timestamp = DateTime.Now;
-            message = String.Concat("  " + message + "  Timestamp: ", timestamp.ToString("F"));
+            _chatMessageRepository = chatMessageRepository;
+        }
+        public async Task SendMessage(string user, string message, string time)
+        {
+            var timestamp = DateTime.ParseExact(time, "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+            message = String.Concat(message + "  ", "Timestamp: " + timestamp.ToString("F"));
             await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task SaveMessage(string user, string message, string time)
+        {
+            var timestamp = DateTime.ParseExact(time, "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+
+            await _chatMessageRepository
+                .SaveChatMessageAsync(  new Models
+                .ChatMessage()
+                { 
+                    DateTime = timestamp,
+                    Message = message,
+                    User = user
+                });
         }
     }
 }
--- src/JobsityChatProject/wwwroot/js/general-chat.js
+++ src/JobsityChatProject/wwwroot/js/general-chat.js
@@ -19,8 +19,30 @@
 
         var user = $('label[for="user"]').text();
         var message = $("#message").val();
-        connection.invoke("SendMessage", user, message).catch(function (err) {
+        
+
+        Number.prototype.padLeft = function (base, chr) {
+            var len = (String(base || 10).length - String(this).length) + 1;
+            return len > 0 ? new Array(len).join(chr || '0') + this : this;
+        }
+
+        var date = new Date();
+        var day = date.getDate();
+        var month = date.getMonth() + 1;
+        var year = date.getFullYear().padLeft(); 
+        var hour = date.getHours().padLeft();     
+        var minute = date.getMinutes().padLeft(); 
+        var second = date.getSeconds().padLeft();
+
+        var time = day + "/" + month.padLeft() + "/" + year + " " + hour + ':' + minute + ':' + second;
+
+        connection.invoke("SendMessage", user, message, time).catch(function (err) {
             return console.error(err.toString());
         });
-        event.preventDefault();
+
+        connection.invoke("SaveMessage", user, message, time).catch(function (err) {
+            return console.error(err.toString());
+        });
+
+        event.preventDefault();        
     });
