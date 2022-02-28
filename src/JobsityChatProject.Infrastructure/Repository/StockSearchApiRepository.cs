8ebea7196af47bd2dc92fbd69e7adf5e451de9b0
Bruno Packers <packersapp2021@outlook.com>
2022-02-28 11:05:43 -0300

    include api repository

---
+++ src/JobsityChatProject.Infrastructure/Repository/StockSearchApiRepository.cs
@@ -0,0 +1,9 @@
+﻿using JobsityChatProject.Core.RepositoryInterfaces;
+
+namespace JobsityChatProject.Infrastructure.Repository
+{
+    public class StockSearchApiRepository : IStockSearchApiRepository
+    {
+
+    }
+}
