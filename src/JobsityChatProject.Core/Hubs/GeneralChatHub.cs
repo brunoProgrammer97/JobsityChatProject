89454ae54e8feb57684427592e6c399b46293d60
Bruno Packers <packersapp2021@outlook.com>
2022-02-27 22:22:34 -0300

    remove constructor from GeneralChatHub

--- src/JobsityChatProject.Core/Hubs/GeneralChatHub.cs
+++ src/JobsityChatProject.Core/Hubs/GeneralChatHub.cs
@@ -1,5 +1,6 @@
 ﻿using JobsityChatProject.Core.RepositoryInterfaces;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Caching.Memory;
 using System;
 using System.Threading.Tasks;
 
@@ -7,11 +8,6 @@ namespace JobsityChatProject.Core.Hubs
 {
     public class GeneralChatHub : Hub
     {
-        private readonly IChatMessageRepository _chatMessageRepository;
-        public GeneralChatHub(IChatMessageRepository chatMessageRepository)
-        {
-            _chatMessageRepository = chatMessageRepository;
-        }
         public async Task SendMessage(string user, string message)
         {
             var timestamp = DateTime.Now;
